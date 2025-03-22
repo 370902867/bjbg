fdb7c4f4cba720fb42b9790b94af392a808d62e5
wangbin9 <wangbin9@asiainfo.com>
2025-03-22 17:47:11 +0800

    add

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
@@ -10,6 +13,16 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner printServerPort(ServletWebServerApplicationContext webServerAppCtxt) {
+		return args -> {
+			int port = webServerAppCtxt.getWebServer().getPort();
+			System.out.println("应用启动成功！");
+			System.out.println("本地访问地址: http://localhost:" + port+"/login.html");
+			System.out.println("外部访问地址: http://" + java.net.InetAddress.getLocalHost().getHostAddress() + ":" + port);
+		};
 	}
 
 }
